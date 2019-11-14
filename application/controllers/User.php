2dd96e715426be7e5236c92e207d8858f2a00009
Muhammad Aulia Asshiddiqie <m.aulia08@mail.unpas.ac.id>
2019-11-15 00:32:52 +0700

    clear design in dashboard

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,8 +3,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class  User extends CI_Controller{
     public function index(){
+        $data['title'] = 'My Profile';
         $data['user'] = $this->db->get_where('user', ['email'=> $this->session->userdata('email')])->row_array();
-        echo 'Selamat Datang ' . $data['user']['name'];
+
+        $this->load->view('user/index', $data);
     }
 
     public function logout(){
