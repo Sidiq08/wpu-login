4e3b385d232a245e904f8821ece769a62b4ea942
Muhammad Aulia Asshiddiqie <m.aulia08@mail.unpas.ac.id>
2019-11-14 23:20:58 +0700

    verify login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,20 +10,56 @@ class Auth extends CI_Controller{
     }
 
     public function index(){
-        $data['title'] = 'WPU Login Page';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/login');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('password','Password', 'trim|required');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'WPU Login Page';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        }else{
+            $this->_login();
+        }
+
+    }
+
+    private function _login(){
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+        if($user){
+            if ($user['is_active'] == 1) {
+                if (password_verify($password, $user['password'])) {
+                    $data =[
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id']
+                    ];
+                    $this->session->set_userdate($data);
+                    redirect('auth');
+                }else{
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong Password!</div>');
+                redirect('auth');
+                }
+            }else{
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This email has not been activated</div>');
+                redirect('auth');
+            }
+        }else{
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is not registered!</div>');
+            redirect('auth');
+        }
     }
 
     public function registration(){
 
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.mail]', [
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
             'is_unique' => "This email has already registered!"
         ]);
 
-        $this->form_validation->set_rules('password','Password', 'required|trim|min_lenght[3]|matches[password2]',[
+        $this->form_validation->set_rules('password','Password', 'required|trim|min_length[3]|matches[password2]',[
             'matches' => 'Password dont match!',
             'min_length'=>'Password to short!'
         ]);
@@ -40,14 +76,15 @@ class Auth extends CI_Controller{
                 'name' => htmlspecialchars($this->input->post('name',true)),
                 'email' => htmlspecialchars($this->input->post('email',true)),
                 'image' => 'default.png',
-                'email'=>password_hash(htmlspecialchars($this->input->post('password')),PASSWORD_DEFAULT),
+                'password'=>password_hash(htmlspecialchars($this->input->post('password')),PASSWORD_DEFAULT),
                 'role_id' => 2,
                 'is_active' => 1,
                 'date_created'=>time(),
             ];
-
             $this->db->insert('user',$data);
-            
-        }
+            $this->session->set_flashdata('message', '<div class="alert alertsuccess" role="alert">Congratulation! your account has been created. Please Login</div>');
+            redirect('auth');
+
     }
+} 
 }
--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -22,10 +22,12 @@
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="password" class="form-control form-control-user" id="password" placeholder="Password">
+                    <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                    <?= form_error('password', '<small class="text-danger pl-3">','</small>'); ?>
                   </div>
                   <div class="col-sm-6">
-                    <input type="password" class="form-control form-control-user" id="password2" placeholder="Repeat Password">
+                    <input type="password" name="password2" class="form-control form-control-user" id="password2" placeholder="Repeat Password">
+                    <?= form_error('password', '<small class="text-danger pl-3">','</small>'); ?>
                   </div>
                 </div>
                 <button type="submit" class="btn btn-primary btn-user btn-block">
